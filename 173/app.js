c3c609096bba8a7b1f278532d06d286314ce2e1d
bharadwajambati172 <bharadwaj.ambati@itpeoplecorp.com>
2019-04-05 11:39:34 -0400

    173

---
+++ 173/app.js
@@ -0,0 +1,27 @@
+let words = ["war", "eat","donut", "pizza", "raw", "ate", "apizz"]
+let result={}
+
+function sortWord(word){
+    let result=word.split("")
+    result=result.sort()
+    return result.join("")
+}
+
+let output=[]
+for(let i in words){
+        result[words[i]]=[]
+        let values=words.filter((x)=>{
+            if(sortWord(x)===sortWord(words[i])){
+                return x
+            }
+        })
+        output.push("("+values.toString()+")")
+        for(let j in values){
+            if(words[i] !== values[j]){
+            let index=words.indexOf(values[j])
+            words.splice(index,1)
+            }
+        }
+}
+output=output.toString()
+console.log(output)
